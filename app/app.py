95bf4e0a97dcd9050ca64ca35754fd8e6047bfaa
Eugene <eugenemutuyimana15@gmail.com>
2025-01-29 15:40:29 +0200

    user registration done

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from app.models import Employee
+from app.models import User, Employee
 from app import db
 from flask import request, abort, make_response
-from flask_restful import Resource, fields, marshal, reqparse
+from flask_restful import Resource, fields, marshal
 from flask import Flask
 from flask_restful import Api
-
+from flask_jwt_extended import create_access_token, get_jwt_identity
 from flask_restful import Resource
 
 
@@ -24,10 +24,13 @@ employee_fields = {
 
 
 class EmployeeResource(Resource):
+    # def __init__(self):
+    #     self.active_user = get_jwt_identity()
+
     def get(self, employee_id=None):
         if employee_id:
             try:
-                employee = Employee.find_user(employee_id)
+                employee = Employee.find_employee(employee_id)
                 if not employee:
                     return make_response({"message": "No employee found"}, 400)
                 return make_response({"message": "Retrieved Successfully", "data": marshal(employee, employee_fields)}, 200)
@@ -67,7 +70,7 @@ class EmployeeResource(Resource):
     def put(self, employee_id=None):
         if not employee_id:
             return make_response({"message": "Please provide an id"}, 400)
-        employee = Employee.find_user(employee_id)
+        employee = Employee.find_employee(employee_id)
 
         if not employee:
             return make_response({"message": "No employee found"}, 404)
@@ -92,19 +95,89 @@ class EmployeeResource(Resource):
     def delete(self, employee_id):
         if not employee_id:
             return {"message": "Provide an employee id"}
-        employee = Employee.find_user(employee_id)
+        employee = Employee.find_employee(employee_id)
         db.session.delete(employee)
         db.session.commit()
-        return make_response({"messaage": "Deleted successfully"}, 204)
+        return make_response(204)
 
 
-class AuthResource(Resource):
-    pass
+class UserResource(Resource):
+    def get(self):
+        pass
+
+    def put(self, user_id=None):
+        if not user_id:
+            return make_response({"message": "Please provide user id"}, 400)
+
+        user = User.find_user(user_id)
+
+        if not user:
+            return make_response({"message": "No user found"}, 404)
+
+        try:
+            for k, v in dict(request.form).items():
+                if hasattr(user, k):
+                    if getattr(user, k) == v:
+                        continue
+                    setattr(user, k, v)
+
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            return make_response({"message": "Something went wrong"}, 400)
+
+        return make_response({"message": "user is updated successfully"}, 200)
+
+    def delete(self, user_id):
+        if not user_id:
+            return {"message": "Provide an user id"}
+        user = User.find_user(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return make_response(204)
+
+
+class UserRegister(Resource):
+    def post(self):
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if (not email) or (not password):
+            abort(400, "Email or Password are not available")
+        try:
+            user = User(email=email, password=password)
+            user.create_user_pass_hash()
+            db.session.add(user)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            return make_response({"message": "User creation failed"}, 400)
+
+        return make_response({"message": "Account created successfully"}, 201)
+
+
+class UserLogin(Resource):
+    def post(self):
+        data = request.form
+        email = data.get('username')
+        password = data.get('password')
+
+        user = User.find_existing_email(email)
+        user_psd = user.check_user_pass_hash(password)
+
+        if (not user) or (not user_psd):
+            return make_response({'message': 'Invalid credentials'}), 401
+
+        access_token = create_access_token(identity=user.id)
+        return make_response({'access_token': access_token, "message": "Access Token generated successfully"}), 200
 
 
 api.add_resource(EmployeeResource,
                  '/employees/',
                  '/employees/<int:employee_id>')
+api.add_resource(UserResource, '/users/', '/auth/<int:user_id>')
+api.add_resource(UserLogin, '/auth/login')
+api.add_resource(UserRegister, '/auth/register')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- app/models.py
+++ app/models.py
@@ -2,6 +2,30 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(
+        autoincrement=True, primary_key=True, unique=True)
+    email: Mapped[str] = mapped_column(String(40), unique=True)
+    password: Mapped[str] = mapped_column(String(256))
+
+    def create_user_pass_hash(self):
+        self.password = generate_password_hash(password=self.password)
+
+    def check_user_pass_hash(self, plain_password):
+        return check_password_hash(self.password, plain_password)
+
+    @staticmethod
+    def find_existing_email(email):
+        return db.session.query(User).filter_by(email=email).first()
+
+    @staticmethod
+    def find_user(id):
+        """Helper to find user in the db"""
+        user = db.session.query(User).filter_by(id=id).first()
+        return user
 
 
 class Employee(db.Model):
@@ -21,7 +45,7 @@ class Employee(db.Model):
         return employee
 
     @staticmethod
-    def find_user(id):
+    def find_employee(id):
         """Helper to find user in the db"""
         employee = db.session.query(Employee).get(id)
         return employee
